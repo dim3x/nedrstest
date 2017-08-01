8e30f3408170c367ecf644dc2709a20a222ad2d6
dim3x <1workdmitriy.t@gmail.com>
2017-08-01 18:49:02 +0300

    Add files via upload

---
+++ index/js/script.js
@@ -0,0 +1,20 @@
+function popup(parentPopup, bgPopup, btnPopup) {
+    var popup = document.querySelector(parentPopup),
+        bg = document.querySelector(bgPopup),
+        btn = document.querySelector(btnPopup);
+
+    btn.addEventListener('click', function(){
+        popup.classList.toggle('visible');
+    });
+    bg.addEventListener('click', function(){
+        popup.classList.toggle('visible');
+    });
+}
+
+popup('.popup', '.popup__bg', '.button-js');
+
+$(document).ready(function(){
+    $('.slider').slick({
+
+    });
+});
